b77872ac7f6dba7c1ca219b3a69243289c9a6d1b
DianaPrunila <pruniladiana03@yahoo.it>
2025-01-10 20:41:33 +0200

    Dashboard Updates

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,141 @@
 const Dashboard = () => {
-  return <div className="text-red-500">Dashboard </div>;
+  return (
+    <div className="dashboard-page">
+      <div className="page-up-info">
+        <div className="left">
+          <h3 className="title-page big">Dashboard</h3>
+          <p className="owner small">Welcome to Prunila finance Management</p>
+        </div>
+        <div className="right">
+          <p className=" small">Home --- Dashboard</p>
+        </div>
+      </div>
+
+      <div className="first-row-frames">
+        <div className="outer-frame-first-row">
+          <div className="total-balance-frame">
+            <p className="total-balance big">Total Balance</p>
+            <p className="sum-total-balance big">$</p>
+            <div className="percentage">
+              <p className="percentage-text">%</p>
+            </div>
+          </div>
+        </div>
+        <div className="outer-frame-first-row">
+          <div className="total-period-change-frame">
+            <p className="total-period-change big">Total Period Change</p>
+            <p className="sum-total-period-change big">$</p>
+            <div className="percentage">
+              <p className="percentage-text">%</p>
+            </div>
+          </div>
+        </div>
+        <div className="outer-frame-first-row">
+          <div className="total-period-expenses-frame">
+            <p className="total-period-expenses big">Total Period Expenses</p>
+            <p className="sum-total-period-expenses big">$</p>
+            <div className="percentage">
+              <p className="percentage-text">%</p>
+            </div>
+          </div>
+        </div>
+        <div className="outer-frame-first-row">
+          <div className="total-period-income-frame">
+            <p className="total-period-income big">Total Period Income</p>
+            <p className="sum-total-period-income big">$</p>
+            <div className="percentage">
+              <p className="percentage-text">%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="outer-frame-balance-trends">
+        <div className="balance-trends-frame">
+          <h4 className="title-balance-trends big">Balance Treends</h4>
+          <div className="balance-trands-frame-up">
+            <p className="period small">Last Month</p>
+            <p className="percentage">%</p>
+          </div>
+          <div className="graph"> graph</div>
+        </div>
+      </div>
+      <div className="outer-frame-monthly-breakdown">
+        <div className="monthly-brakdown-frame">
+          <h4 className="monthly-brakdown-title big">
+            Monthly Expense Breakdown
+          </h4>
+          <div className="bar-expense">bar expense</div>
+          <div className="catergories">
+            <div className="dots">dot </div>
+            <div className="category-name-div small">
+              <p className="category-name-title"> FOOD</p>
+            </div>
+            <div className="right-breakdown">
+              <p className="category-sum">$</p>
+              <p className="category-percentage">%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="outer-frame-monthly-budgets">
+        <div className="monthly-budgets-frame">
+          <div className="monthly-budgets-title-frame">
+            <h4 className="monthly-budget-title big">Monthly Budget</h4>
+          </div>
+          <div className="budgets-categories">
+            <div className="categ-1">
+              <div className="logo">logo</div>
+              <div className="categ-writing">
+                <p className="">Grocery Stores</p>
+                <p className="slash">75/100</p>
+              </div>
+              <div className="bar">bar</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="outer-frame-income-vs-expenses">
+        <div className="income-vs-expenses-frame">
+          <div className="income-vs-expenses-title-frame">
+            <h4 className="income-vs-expenses-title big">
+              Monty Income vs Expenses
+            </h4>
+          </div>
+          <div className="income-vs-expenses-graph">Graph</div>
+        </div>
+      </div>
+      <div className="outer-frame-weekly-expenses">
+        <div className="weekly-expenses-frame">
+          <div className="weekly-expenses-title-frame">
+            <h4 className="weekly-expenses-title big">Weekly Expenses</h4>
+          </div>
+          <div className="weekly-expenses-graph">Graph</div>
+        </div>
+      </div>
+      <div className="outer-frame-payements-history">
+        <div className="frame-payement-history">
+          <div className="frame-payement-history-title">
+            <h4 className="payement-history-title big">Payement History</h4>
+            <p>See more</p>
+          </div>
+          <div className="payement-history-categories">
+            <div className="categ -1">
+              <div className="frame-categ-1-tile+time">
+                <h5 className="caterg-1-title">Electity</h5>
+                <p className="categor-1-time">January</p>
+              </div>
+              <div className="frame-categ-1-sum+state">
+                <h5 className="caterg-1-sum">+450.00</h5>
+                <div className="pad ">
+                  <p className="categor-1-state">Paid</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
