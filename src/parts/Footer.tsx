9e3cbfb7642be5d63f11ca75502d7145ef24f8a2
DianaPrunila <pruniladiana03@yahoo.it>
2025-01-14 18:36:07 +0200

    Footer + NavBar

--- src/parts/Footer.tsx
+++ src/parts/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { MDBFooter } from "mdb-react-ui-kit";
+
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -7,37 +7,39 @@ import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <MDBFooter className="flex  justify-between  bg-bkGd text-lg-start text-small ">
-      <div className=" text-center p-4 ">
-        © Copyright 2025{" "}
-        <a className="text-reset fw-bold" href="/">
-          Diana{" "}
-        </a>
-        I All Rights Reserved
+    <footer className="flex fixed w-full justify-between bg-bkGd text-lg text-small ">
+      <div className="flex ">
+        <div className=" copy-right text-center p-4 ">
+          © Copyright 2025{" "}
+          <a className="text-reset fw-bold" href="/">
+            Diana{" "}
+          </a>
+          I All Rights Reserved
+        </div>
+        <div className="footer-icons flex  items-center text-small icons">
+          <Link to="https://www.facebook.com/">
+            <div className="fb">
+              <FaFacebook />
+            </div>
+          </Link>
+          <Link to="https://x.com/home">
+            <div className="twitter">
+              <FaTwitter />
+            </div>
+          </Link>
+          <Link to="https://www.linkedin.com/">
+            <div className="likedIn">
+              <FaLinkedin />
+            </div>
+          </Link>
+          <Link to="https://www.youtube.com/">
+            <div className="YT">
+              <FaYoutube />
+            </div>
+          </Link>
+        </div>
       </div>
-      <div className=" flex items-center text-small icons">
-        <Link to="https://www.facebook.com/">
-          <div className="fb">
-            <FaFacebook />
-          </div>
-        </Link>
-        <Link to="https://x.com/home">
-          <div className="twitter">
-            <FaTwitter />
-          </div>
-        </Link>
-        <Link to="https://www.linkedin.com/">
-          <div className="likedIn">
-            <FaLinkedin />
-          </div>
-        </Link>
-        <Link to="https://www.youtube.com/">
-          <div className="YT">
-            <FaYoutube />
-          </div>
-        </Link>
-      </div>
-    </MDBFooter>
+    </footer>
   );
 };
 
--- src/parts/NavBar.tsx
+++ src/parts/NavBar.tsx
@@ -1,5 +1,38 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import { FaSearch } from "react-icons/fa";
+import { IoSunny } from "react-icons/io5";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { FaRegUserCircle } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <nav className=" flex  bg-bkGd fixed w-full">
+      <div className="nav-left flex">
+        <form className="flex flex-col items-start">
+          <input
+            type="text"
+            placeholder="Search Here"
+            className=" p-2 text-base  border-2 border-sidebarColor rounded-md focus:outline-none  "
+          />
+        </form>
+        <div className=" text-3xl text-white">
+          <button className="bg-sidebarColor ">
+            <FaSearch />
+          </button>
+        </div>
+      </div>
+      <div className="nav-right flex text-2xl text-small">
+        <div className="sun">
+          <IoSunny />
+        </div>
+        <div className="notification">
+          <IoMdNotificationsOutline />
+        </div>
+        <div className="user-nav">
+          <FaRegUserCircle />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar;
+export default NavBar;
