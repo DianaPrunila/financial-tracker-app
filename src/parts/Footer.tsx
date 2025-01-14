771e5f640cb1339f9ad8677ab4b0d4efb189732f
DianaPrunila <pruniladiana03@yahoo.it>
2025-01-14 14:27:50 +0200

    Footer links

--- src/parts/Footer.tsx
+++ src/parts/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { MDBFooter } from "mdb-react-ui-kit";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
@@ -8,26 +9,33 @@ const Footer = () => {
   return (
     <MDBFooter className="flex  justify-between  bg-bkGd text-lg-start text-small ">
       <div className=" text-center p-4 ">
-        © Copyright 2025
+        © Copyright 2025{" "}
         <a className="text-reset fw-bold" href="/">
-          Diana
+          Diana{" "}
         </a>
         I All Rights Reserved
       </div>
       <div className=" flex items-center text-small icons">
-        <div className="fb">
-          <FaFacebook />
-        </div>
-        <div className="twitter">
-          <FaTwitter />
-        </div>
-
-        <div className="likedIn">
-          <FaLinkedin />
-        </div>
-        <div className="YT">
-          <FaYoutube />
-        </div>
+        <Link to="https://www.facebook.com/">
+          <div className="fb">
+            <FaFacebook />
+          </div>
+        </Link>
+        <Link to="https://x.com/home">
+          <div className="twitter">
+            <FaTwitter />
+          </div>
+        </Link>
+        <Link to="https://www.linkedin.com/">
+          <div className="likedIn">
+            <FaLinkedin />
+          </div>
+        </Link>
+        <Link to="https://www.youtube.com/">
+          <div className="YT">
+            <FaYoutube />
+          </div>
+        </Link>
       </div>
     </MDBFooter>
   );
