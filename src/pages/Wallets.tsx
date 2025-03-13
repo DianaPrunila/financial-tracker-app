d3904d2ecadfd908eceebef95a58a2d4ed488455
DianaPrunila <pruniladiana03@yahoo.it>
2025-03-13 19:13:22 +0200

    Wallets

--- src/pages/Wallets.tsx
+++ src/pages/Wallets.tsx
@@ -1,5 +1,21 @@
+import { Container } from "react-bootstrap";
+
 const Wallets = () => {
-  return <div>Wallets</div>;
+  return (
+    <Container id="dashboard-page" className="-mt-4 pb-3">
+      <div id="page-up-info" className="mr-3 flex flex-col pt-4">
+        <div id="dashboard-up" className="-ml-3">
+          <h3 id="title-page">Dashboard</h3>
+        </div>
+        <div
+          id="dashboard-down"
+          className="-ml-3 mb-5 mt-3 flex justify-between"
+        >
+          <p id="owner-page">Welcome to Vanja finance Management</p>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Wallets;
