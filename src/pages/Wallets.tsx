5897d0d2e382ecc3670e4927503b473a24941cc6
DianaPrunila <pruniladiana03@yahoo.it>
2025-03-17 18:25:02 +0200

    walletsLeft

--- src/pages/Wallets.tsx
+++ src/pages/Wallets.tsx
@@ -1,8 +1,23 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import TraHi from "../parts/TraHi";
+import { useEffect, useState } from "react";
 
 const Wallets = () => {
+  const [crd, setCrd] = useState<Crd[]>([]);
+  interface Crd {
+    id: number;
+    title: string;
+    currency: symbol;
+    amount: number;
+  }
+  useEffect(() => {
+    fetch("/data/cards.json")
+      .then((response) => response.json())
+      .then((response) => setCrd(response))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
   return (
-    <Container id="dashboard-page" className="-mt-4 pb-3">
+    <Container id="wallets-page" className="-mt-4 pb-3">
       <div id="page-up-info" className="mr-3 flex flex-col pt-4">
         <div id="dashboard-up" className="-ml-3">
           <h3 id="title-page">Dashboard</h3>
@@ -14,6 +29,32 @@ const Wallets = () => {
           <p id="owner-page">Welcome to Vanja finance Management</p>
         </div>
       </div>
+      <Row id="bigRow" className="w-full">
+        <Col id="walletsLeft" className="">
+          {crd.map((c) => (
+            <Row
+              id=""
+              className="mb-4 h-[5.69rem] w-[53%] rounded-md bg-white p-4 shadow-md hover:bg-sidebarColor hover:text-white"
+              key={c.id}
+            >
+              <div id="cardContent" className="flex flex-col">
+                <div id="cardContent-Left"></div>
+                <div id="cardContent-Right" className="j flex flex-col">
+                  <h3 id="cardTitle" className="text-lg">
+                    {c.title}
+                  </h3>
+                  <span id="cardTitle">${c.amount}</span>
+                </div>
+              </div>
+            </Row>
+          ))}
+        </Col>
+        <Col id="walltsRight" className=""></Col>
+      </Row>
+      <Row id="BalanceOvertime-outer-frame"></Row>
+      <Row>
+        <TraHi />
+      </Row>
     </Container>
   );
 };
