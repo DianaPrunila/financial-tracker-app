50c4f3278de424b5ff14c0b5e72b8e52692d232c
DianaPrunila <pruniladiana03@yahoo.it>
2025-03-31 16:49:00 +0300

    Budgets page

--- src/pages/Budgets.tsx
+++ src/pages/Budgets.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
-import { Col, Container, Dropdown, Row } from "react-bootstrap";
-import { AiOutlineRise } from "react-icons/ai";
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { GoDotFill } from "react-icons/go";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import BPBarchart from "../parts/BPBarchart";
 
 const Budgets = () => {
-  const [budgetP, setBudgetP] = useState<BudgetPeriod[]>([]);
   const [cardsBL, setCardsBL] = useState<CardsBudsgetsLeft[]>([]);
   interface CardsBudsgetsLeft {
     id: number;
@@ -13,19 +13,7 @@ const Budgets = () => {
     amount: number;
     time: string;
   }
-  interface BudgetPeriod {
-    id: number;
-    month: string;
-    a: number;
-    b: number;
-    c: number;
-  }
-  useEffect(() => {
-    fetch("/data/budgetPeriod.json")
-      .then((response) => response.json())
-      .then((response) => setBudgetP(response))
-      .catch((error) => console.error("Error fetching data:", error));
-  });
+
   useState(() => {
     fetch("/data/cardsBudgetsLeft.json")
       .then((response) => response.json())
@@ -74,71 +62,77 @@ const Budgets = () => {
         <Col id="budgetsRight">
           <Row
             id="budgetTitle"
-            className="rounded-md bg-white py-3 pl-2 shadow-md"
+            className="mb-4 rounded-md bg-white py-3 pl-2 shadow-md"
           >
             <h3>Grocery</h3>
           </Row>
 
-          <Row id="budgetsCards-row-up" className="gap-x-8">
-            <Col
-              id="budgetsCards-col-up-big-left"
-              className="mt-[1.875rem] flex flex-col justify-between rounded-md bg-white p-4 shadow-md"
+          <Row
+            id="budgets-secondRow-up"
+            className="mb-4 w-[103.5%] rounded-md bg-white px-3 py-4 shadow-md"
+          >
+            <div id="budgets-secondRow-up-up" className="flex justify-between">
+              <p>Spend</p>
+              <p>Budget</p>
+            </div>
+            <div
+              id="budgets-secondRow-up-middle"
+              className="mb-2 flex justify-between"
             >
-              <div>
-                <h6>Total Balance</h6>
-                <h2 className="mb-2 leading-[3rem]">$221,478</h2>
-              </div>
-              <div>
-                <div className="flex justify-between py-2">
-                  <p>Personal Funds</p>
-                  <p className="text-base font-bold text-big">$32,500.28</p>
-                </div>
-                <Dropdown.Divider />
-                <div className="flex justify-between pt-2">
-                  <p>Credit Limits</p>
-                  <p className="text-base font-bold text-big">$2500.00</p>
-                </div>
-              </div>
-            </Col>
+              <span className="text-2xl font-medium leading-[2.4rem] text-big">
+                $1458.30
+              </span>
+              <span className="text-2xl font-medium leading-[2.4rem] text-big">
+                $1458.30
+              </span>
+            </div>
+            <div id="budgets-secondRow-up-progressBar" className="pb-2">
+              <ProgressBar now={25} className="h-2 rounded-full" />
+            </div>
           </Row>
 
-          <Row id="budgetsCards-row-down" className="mb-4 gap-x-8">
-            <Col
-              id="walletsCards-col-down-big-left"
-              className="mt-[1.875rem] rounded-md bg-white p-4 shadow-md"
-            >
-              <div>
-                <h6>Total Balance</h6>
-                <p className="my-3 text-3xl font-medium text-big">$ 432568</p>
-              </div>
-
-              <Dropdown.Divider />
-              <div id="percentage-1" className="flex items-center gap-1 pt-2.5">
-                <span className="text-xl text-riseingPercentageColor">
-                  <AiOutlineRise />
+          <Row
+            id="budgets-thirdRow"
+            className="mb-4 w-[103.5%] rounded-md bg-white px-3 py-4 shadow-md"
+          >
+            <div className="flex justify-between">
+              <div id="budgets-thirdRow-1" className="">
+                <span className="">Last Month</span>
+                <span className="mt-3 flex text-2xl font-medium text-big">
+                  $42,678
                 </span>
-                <p id="percentage-text-1" className=" ">
-                  2.47% Last month <span className="font-bold">$24,478</span>
-                </p>
               </div>
-            </Col>
-            <Col
-              id="walletsCards-col-down-big-left"
-              className="mt-[1.875rem] rounded-md bg-white p-4 shadow-md"
-            >
-              <div>
-                <h6>Monthly Expenses</h6>
-                <p className="my-3 text-3xl font-medium text-big">$ 432568</p>
-              </div>
-
-              <Dropdown.Divider />
-              <div id="percentage-1" className="flex items-center gap-1 pt-2.5">
-                <span className="text-xl text-riseingPercentageColor">
-                  <AiOutlineRise />
+              <div id="budgets-thirdRow-2" className="">
+                <span className="">Expenses</span>
+                <span className="mt-3 flex text-2xl font-medium text-big">
+                  $1,798
                 </span>
-                <p id="percentage-text-1" className=" ">
-                  2.47% Last month <span className="font-bold">$24,478</span>
-                </p>
+              </div>
+              <div id="budgets-thirdRow-3" className="">
+                <span className="">Taxes</span>
+                <span className="mt-3 flex text-2xl font-medium text-big">
+                  $255.25
+                </span>
+              </div>
+              <div id="budgets-thirdRow-4" className="">
+                <span className="">Debt</span>
+                <span className="mt-3 flex text-2xl font-medium text-big">
+                  $365,478
+                </span>
+              </div>
+            </div>
+          </Row>
+
+          <Row
+            id="budgets-forthRow"
+            className="w-[103.5%] rounded-md bg-white px-3 py-4 shadow-md"
+          >
+            <Col id="budget-period-col" xs={12} lg={8}>
+              <div id="title-budget-period">
+                <h4 className="mb-4"> Budget Period</h4>
+              </div>
+              <div id="budgetPeriod-chart" className="-ml-10 h-[83%]">
+                <BPBarchart />
               </div>
             </Col>
           </Row>
